6fe3390fda28bfbadd95e8a24899122e19a34610
Saksham Saxena <chatarpansy@gmail.com>
2026-02-23 08:55:49 +0530

    Replace isPowerOfTwo with power function

--- D8Q1.c
+++ D8Q1.c
@@ -1,5 +1,14 @@
-#include <stdbool.h>
+#include <stdio.h>
 
-bool isPowerOfTwo(int n){
-    return n>0 && (n&(n-1))==0;
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", power(a, b));
+    return 0;
 }
