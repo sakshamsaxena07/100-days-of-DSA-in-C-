a0c490a2dd44040d2565d5e4ec6f28b2b45f945b
Saksham Saxena <chatarpansy@gmail.com>
2026-02-23 08:48:07 +0530

    Refactor Fibonacci function and main for readability

--- D7Q1.c
+++ D7Q1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int fib(int n){
-    if(n<=1)
+int fib(int n) {
+    if (n <= 1)
         return n;
-    return fib(n-1)+fib(n-2);
+    return fib(n-1) + fib(n-2);
 }
 
-int main(){
+int main() {
     int n;
-    scanf("%d",&n);
-    printf("%d",fib(n));
+    scanf("%d", &n);
+    printf("%d", fib(n));
+    return 0;
 }
