97c5dfccb110375f54764faf37acdf3b12ca8dd3
Saksham Saxena <chatarpansy@gmail.com>
2026-02-23 08:59:10 +0530

    Update D9Q2.c

--- D9Q2.c
+++ D9Q2.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char s[100];
-    scanf("%s",s);
+void reverse(char s[], int n) {
+    int left = 0, right = n - 1;
+    char temp;
 
-    int l=0,r=strlen(s)-1;
-    int ok=1;
-
-    while(l<r){
-        if(s[l]!=s[r]){
-            ok=0;
-            break;
-        }
-        l++; r--;
+    while (left < right) {
+        temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
     }
+}
 
-    printf(ok?"YES":"NO");
+int main() {
+    char s[100];
+    scanf("%s", s);
+
+    int n = strlen(s);
+    reverse(s, n);
+
+    printf("%s", s);
+    return 0;
 }
