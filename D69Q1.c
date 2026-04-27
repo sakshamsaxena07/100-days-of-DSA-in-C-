f1f9a0e51f9f62f3067880f1a441660384bd279d
Saksham Saxena <chatarpansy@gmail.com>
2026-04-27 11:42:12 +0530

    Create D69Q1.c

---
+++ D69Q1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define MAX 1000
+#define INF 1e9
+
+int minDistance(int dist[], int visited[], int n) {
+    int min = INF, index = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i] && dist[i] < min) {
+            min = dist[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void dijkstra(int n, int graph[MAX][MAX], int src) {
+    int dist[MAX], visited[MAX];
+
+    for (int i = 0; i < n; i++) {
+        dist[i] = INF;
+        visited[i] = 0;
+    }
+
+    dist[src] = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        int u = minDistance(dist, visited, n);
+        visited[u] = 1;
+
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    // print result
+    for (int i = 0; i < n; i++)
+        printf("%d ", dist[i]);
+}
